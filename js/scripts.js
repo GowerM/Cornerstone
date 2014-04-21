eb847c8ea40e8a8e2f3a8182e3d48df665146fb9
Mollie <eg.mollie@gmail.com>
2014-04-21 14:27:59 -0700

    Figuring out nav

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
 new Maplace({
 	show_markers: true,
 	locations: [{
-		lat: 47.489406, 
-		lon: -117.580053,
+		lat: 47.652177, 
+		lon: -117.34789,
 		zoom: 17
 			}]
 		}).Load(); 
@@ -21,5 +21,3 @@ new Maplace({
 
 
 
-//s.google.com/maps?q=200+E.+2ND+AVENUE+SPOKANE,+WA++99202&hl=en&sll=47.489406,-117.580053&sspn=0.131076,0.125999&hnear=200+E+2nd+Ave,+Spokane,+Spokane+County,+Washington+99202&t=m&z=17
-//https://maps.google.com/maps?q=765+E+Holland+Avenue+Spokane,+WA+99218&hl=en&sll=47.654254,-117.407849&sspn=0.008166,0.007875&hnear=765+E+Holland+Ave,+Spokane,+Washington+99218&t=m&z=17
